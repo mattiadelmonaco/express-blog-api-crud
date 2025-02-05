5a11706d2c3eed337a1cfa9f65095298496b7a91
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-05 15:40:47 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 // imports
 
-const posts = require("./data/postsData")
-const postsRouter = require("./routers/postsRouter")
-const notFound = require("./middlewares/notFound")
-const errorHandlerServer = require("./middlewares/errorHandlerServer")
+const posts = require("./data/postsData");
+const postsRouter = require("./routers/postsRouter");
+const notFound = require("./middlewares/notFound");
+const errorHandlerServer = require("./middlewares/errorHandlerServer");
+const cors = require("cors");
 
 // Local server with express.js
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 // to get static files
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // to decode the request-body for application/json
-app.use(express.json())
+app.use(express.json());
 
 // routers
-app.use("/posts", postsRouter)
+app.use("/posts", postsRouter);
 
 // server's errors
-app.use(errorHandlerServer)
+app.use(errorHandlerServer);
 
 // route not found
-app.use(notFound)
+app.use(notFound);
 
 // console.log to show the using port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
